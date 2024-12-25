e3a5827b2e557fe455ae086ed63b2cf79c2078d9
CotziCoder <cotzicoder@gmail.com>
2024-12-25 17:30:47 +0530

    Overlay Navbar
    OverLay full screen Navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('navbar').style.height = "0%"
+function close11() {
+    document.getElementById('navbar').style.height = "0%"
+}
+
+function open11() {
+    document.getElementById('navbar').style.height = "100%"
+}
